3b51a721ca976bad5e888e07d870e6f65e9b872b
vivycahyani <101357509+vivycahyani@users.noreply.github.com>
2022-05-05 01:55:32 +0700

    js-switch statemet + kepoin webstorm + instal extension live preview, peacock, quokka

---
+++ scripts/coba.js
@@ -0,0 +1,11 @@
+for (let i = 0; i < 10; i++){
+    console.log("Hello World");
+}
+console.log(1+1);
+
+/*
+Cara menggunakan Quokka :
+buka command pallete (ctrl+shift+p)
+ketik quokka
+pilih quokka start on current file
+*/
